f4f9372cb523b287ed8a18b6db88e713d0baa13e
Sven Ehrke <sven.ehrke@canoo.com>
2013-02-24 12:11:36 +0100

    first primitive usage analysis

--- src/org/svenehrke/intellij/plugin/cohesion/CohesionAnalyzer.java
+++ src/org/svenehrke/intellij/plugin/cohesion/CohesionAnalyzer.java
@@ -3,9 +3,10 @@ package org.svenehrke.intellij.plugin.cohesion;
 import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiJavaFile;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.*;
+import com.intellij.psi.search.searches.ReferencesSearch;
+
+import java.util.Collection;
 
 public class CohesionAnalyzer {
 
@@ -39,6 +40,49 @@ public class CohesionAnalyzer {
 				PsiJavaFile javaFile = (PsiJavaFile) psiFile;
 				final String packageName = javaFile.getPackageName();
 				output.addResult(new AnalysisResult(packageName));
+
+				for (PsiClass psiClass : javaFile.getClasses()) {
+					output.addResult(new AnalysisResult("- " + psiClass.getName()));
+
+					for (PsiField psiField : psiClass.getFields()) {
+						final Collection<PsiReference> usages = ReferencesSearch.search(psiField).findAll();
+						output.addResult(new AnalysisResult("-- " + psiField.getName() + " (" + usages.size() + ")"));
+
+						for (PsiReference usage : usages) {
+							final String s = usage.toString() + ", " + usage.getClass().getSimpleName();
+							output.addResult(new AnalysisResult("--- " + s));
+							PsiReferenceExpression rx = (PsiReferenceExpression) usage;
+							PsiElement parent = rx.getParent();
+							while (!(parent instanceof PsiFile) && (parent != null)) {
+//								output.addResult(new AnalysisResult("---- parent: " + parent.toString()));
+								if (parent instanceof PsiMethod) {
+									PsiMethod method = (PsiMethod) parent;
+									output.addResult(new AnalysisResult("---- used in: " + method.getName()));
+								}
+								parent = parent.getParent();
+							}
+						}
+					}
+
+
+/*
+					for (PsiMethod psiMethod : psiClass.getMethods()) {
+						output.addResult(new AnalysisResult("- " + psiMethod.getName()));
+						final PsiCodeBlock body = psiMethod.getBody();
+
+						for (PsiStatement psiStatement : body.getStatements()) {
+							if (psiClass instanceof PsiDeclarationStatement) {
+								final PsiDeclarationStatement psiDeclarationStatement = (PsiDeclarationStatement) psiClass;
+								final String s = psiDeclarationStatement.getFirstChild().getClass().getSimpleName();
+								output.addResult(new AnalysisResult("-- " + s));
+
+							}
+						}
+					}
+*/
+
+				}
+
 			}
 		}
 	}
---
+++ src/org/svenehrke/intellij/plugin/cohesion/Dummy.java
@@ -0,0 +1,12 @@
+package org.svenehrke.intellij.plugin.cohesion;
+
+public class Dummy {
+
+	private String f1;
+
+	public void mmm() {
+		String t;
+		f1 = "+";
+
+	}
+}
